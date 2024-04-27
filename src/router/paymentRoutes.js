4732651eae9c0687e87aec5ac148f8c9dc715d41
João Génio <jgenio2@gmail.com>
2024-04-27 18:25:45 +0200

    add nobel configs

--- src/router/paymentRoutes.js
+++ src/router/paymentRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const stripe = require('stripe')(
-  'sk_test_51MhWh3KFBbgW0VClXuy06geu6WV84abzUKr4hYANWWILR8ClcVnKDoMtbK17islkfqhVIvgUPxM657H9ybvl8hzp00jN2pCpSm'
+  'sk_test_51PA7phAPkcY5p1gtBDEcrG2SUPncgCEawrNAljmMRZhJP9Led5nfKAm2YGS5jliHg5NmO2dc1EtKodzWjF8u6WWZ006chYuNnH'
 );
 
 // router endpoints
@@ -10,10 +10,8 @@ router.post('/intents', async (req, res) => {
     // create a PaymentIntent
     const paymentIntent = await stripe.paymentIntents.create({
       amount: req.body.amount, // Integer, usd -> pennies, eur -> cents
-      currency: 'usd',
-      automatic_payment_methods: {
-        enabled: true,
-      },
+      currency: 'eur',
+      payment_method_types: ['bancontact', 'card', 'paypal'],
     });
     // Return the secret
     res.json({ paymentIntent: paymentIntent.client_secret });
